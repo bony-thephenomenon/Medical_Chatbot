a556152b5696a6e8bb020cefe252ad6408b43c28
Bony Biswas <bonybiswas2311@gmail.com>
2026-03-29 18:14:32 +0530

    fixed port for render

--- app.py
+++ app.py
@@ -97,6 +97,7 @@ Answer ONLY about that disease.
     )
 
     return str(answer)
+import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
